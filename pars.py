9a2d0d39f18e81d5f83d6a17e2cf9c6aac1399fa
Jud <yegoryflo@gmail.com>
2022-04-06 21:23:44 +0300

    Rename atributes

--- pars.py
+++ pars.py
@@ -3,14 +3,16 @@ import os
 import click
 
 from bs4 import BeautifulSoup
+
 headers = {
     "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:97.0) Gecko/20100101 Firefox/97.0"}
 
+
 @click.command()
-@click.option("--tn", prompt="Track name", help="Write track name")
-def tn(tn):
-    url=f"https://ru.hitmotop.com/search?q={tn}"
-    r = requests.get(url, headers=headers,timeout=15)
+@click.option("--track_name", prompt="Track name", help="Write track name")
+def tn(track_name):
+    url = f"https://ru.hitmotop.com/search?q={track_name}"
+    r = requests.get(url, headers=headers, timeout=15)
     soup = BeautifulSoup(r.content, "html.parser")
     src = soup.find("li", class_="tracks__item track mustoggler")
     title = src.find("a", class_="track__info-l").find("div", class_="track__title").text.strip()
@@ -21,12 +23,13 @@ def tn(tn):
 
 
 def download_song(link, title):
-    requst_song = requests.get(link, timeout=15)
-    if os.path.isfile(f"/home/jud/Music/{title}.mp3") == True: 
+    response_song = requests.get(link, timeout=15)
+    if os.path.isfile(f"/home/jud/Music/{title}.mp3"):
         return 0
+
     print("Скачиваю")
     with open(f"/home/jud/Music/{title}.mp3", 'wb+') as f:
-        f.write(requst_song.content)
+        f.write(response_song.content)
 
 
 if __name__ == "__main__":
