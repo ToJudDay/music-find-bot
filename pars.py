0af8de68f398b2080a67b3d3765b8fd60dee54a3
Jud <yegoryflo@gmail.com>
2022-04-06 20:48:51 +0300

    review code

--- pars.py
+++ pars.py
@@ -1,50 +1,33 @@
 import requests
-from bs4 import BeautifulSoup
+import os
+import click
 
+from bs4 import BeautifulSoup
 headers = {
     "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:97.0) Gecko/20100101 Firefox/97.0"}
 
-
-def get_data(url):
-    itertion_count = 0
-    r = requests.get(url)
-    with open("index.html", "w") as file:
-        file.write(r.text)
-    soup = BeautifulSoup(r.text, "lxml")
-    src = soup.find_all("li", class_="tracks__item track mustoggler")
-    for content in src:
-        if itertion_count == 1:
-            print("5 tracks shown")
-            break
-        title = content.find(
-            "a", class_="track__info-l").find("div", class_="track__title").text.strip()
-        # print(title)
-        artist = content.find("div", class_="track__desc").text
-        link = content.find("a", class_="track__download-btn").get("href")
-        print(
-            f"Track name: {title} \nArtist: {artist} \nLink for download: {link}\n ")
-        itertion_count += 1
-        download_song(link, title, artist)
-        # wget.download(link)
+@click.command()
+@click.option("--tn", prompt="Track name", help="Write track name")
+def tn(tn):
+    url=f"https://ru.hitmotop.com/search?q={tn}"
+    r = requests.get(url, headers=headers,timeout=15)
+    soup = BeautifulSoup(r.content, "html.parser")
+    src = soup.find("li", class_="tracks__item track mustoggler")
+    title = src.find("a", class_="track__info-l").find("div", class_="track__title").text.strip()
+    link = src.find("a", class_="track__download-btn").get("href")
+    artist = src.find("div", class_="track__desc").text
+    download_song(link, title)
+    print(f"Track name: {title} \nArtist: {artist.lower()} \nLink for download: {link}")
 
 
-def download_song(link, title, artist):
-    # full_link = f"https://musify.club{link}"
-    requst_song = requests.get(link, timeout=10)
-    # track_save = open(f"{title} - {artist}.mp3", "wb")
-    # # track_save.write(requst_song.content)
-    # with open(f"{title} - {artist}.mp3", 'wb') as file:
-    #     for chunk in requst_song.iter_content(chunk_size=1024 * 1024):
-    #         if chunk:
-    #             file.write(chunk)
-    with open(f"tracks/{title} - {artist}.mp3", 'wb') as f:
+def download_song(link, title):
+    requst_song = requests.get(link, timeout=15)
+    if os.path.isfile(f"/home/jud/Music/{title}.mp3") == True: 
+        return 0
+    print("Скачиваю")
+    with open(f"/home/jud/Music/{title}.mp3", 'wb+') as f:
         f.write(requst_song.content)
 
 
-def main():
-    track = input("Write track name: ")
-    get_data(url=f"https://ru.hitmotop.com/search?q={track}")
-
-
 if __name__ == "__main__":
-    main()
+    tn()
